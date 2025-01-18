eec2d427bd38842e2a21bce11d182912c2493c86
bgautham4 <bgautham4@gmail.com>
2025-01-18 13:42:58 +0530

    Script for getting model memory usage profile

---
+++ benchmarks/test_usage.py
@@ -0,0 +1,43 @@
+from vllm import LLM
+from vllm.utils import FlexibleArgumentParser
+
+
+# Create an LLM with desired params to see memory profile
+def main(args):
+    llm = LLM(model=args.model,
+              max_model_len=args.max_model_len,
+              max_num_seqs=args.max_num_seqs,
+              max_num_batched_tokens=args.max_num_batched_tokens,
+              gpu_memory_utilization=1.0  # Use all of GPU
+              )
+
+
+if __name__ == '__main__':
+    parser = FlexibleArgumentParser(
+        description='Profile model memory usage',
+    )
+    parser.add_argument(
+        '--model',
+        type=str,
+        required=True,
+        help='Name of model.'
+    )
+    parser.add_argument(
+        '--max-model-len',
+        type=int,
+        default=2048,
+        help='Max model len.'
+    )
+    parser.add_argument(
+        '--max-num-seqs',
+        type=int,
+        default=100,
+        help='Max number of seqs for parallel decode.'
+    )
+    parser.add_argument(
+        '--max-num-batched-tokens',
+        type=int,
+        default=2048,
+        help='Max no of tokens for parallel prefill/decode.'
+    )
+    main(parser.parse_args())
