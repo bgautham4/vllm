6c4f8a59c1bde8e75527bd3341c40b1b536bed00
bgautham4 <bgautham4@gmail.com>
2025-09-11 16:07:46 +0530

    minor changes

--- benchmarks/exp_prefill_and_decode.sh
+++ benchmarks/exp_prefill_and_decode.sh
@@ -19,8 +19,14 @@ cd "${0%/*}" || exit 1
 trap 'pkill exp.sh; exit 1' SIGTERM SIGINT
 
 for ((ilen=8; ilen<=TB/2; ilen*=2)); do
+        res_dir="./results/logs_npt_${ilen}"
+        if [ -d "$res_dir" ]; then 
+                rm "$res_dir"/*
+        else
+                mkdir "$res_dir"
+        fi
         for ((i=1; i<TB-ilen; i+=5)); do
                 ./run.sh --model "$MODEL" --token-budget "$TB" --max-num-seqs "$((i + 1))" --num-prompts 5000 --ilen "$ilen" --olen "$((i + 10))" -- --profile-scheduler --time-model
-                mv logs/vllm_logs.jsonl "./results/log_tb_${TB}_dbs_${i}_npt_${ilen}.jsonl"
+                mv logs/vllm_logs.jsonl "${res_dir}/log_${i}.jsonl"
         done
 done
